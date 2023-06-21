c6e7152e0984b0b2eebaa4418f1c0bc6462b27fd
facuolivamar <facundoolivam@gmail.com>
2023-06-20 21:30:22 -0300

    data into html

--- Code/interface/interfaceProject/interfaceApp/views.py
+++ Code/interface/interfaceProject/interfaceApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+# from .models import CSVData
+
+def csv_data_view(request):
+    # data = CSVData.objects.all()
+    data = 'all okey'
+    return render(request, 'interfaceApp/csv_data.html', {'data': data})
