4358dcd47b0197f5add625d238a0f05c090bc46b
facuolivamar <facundoolivam@gmail.com>
2023-06-22 11:15:16 -0300

    updated data from serial

--- Code/interface/dataStorage.py
+++ Code/interface/dataStorage.py
@@ -1,19 +1,39 @@
 import serial
 import pandas as pd
+import re
 
-ser = serial.Serial('COM3', 115200, timeout=1)
+try:
+    # Open a connection to the serial port
+    ser = serial.Serial('COM11', 115200, timeout=1)
 
-line = ser.read(10)
-print(line)
+    # Read data from the serial port
+    if not ser.isOpen():
+        ser.open()
 
-data = []
-while True:
-    # line = ser.readline().decode().strip()
-    line = ser.read(10)
-    data.append(line)
-    print(data)
-    if len(data) >= 100:
-        break
+    data = ser.read(1000).decode('utf-8')
 
-df = pd.DataFrame(data, columns=['Data'])
-df.to_csv('data.csv', index=False)
+    # print(f"Received data: {data}")
+
+    temperaturePattern = re.compile(r"Temperature = (\d\d.\d\d)")
+    temperature = temperaturePattern.search(data).group(1)
+
+    pressurePattern = re.compile(r"Pressure = (\d\d\d\d\d)")
+    pressure = pressurePattern.search(data).group(1)
+
+    altitudePattern = re.compile(r"Altitude = (\d\d\d.\d\d)")
+    altitude = altitudePattern.search(data).group(1)
+
+    rssiPattern = re.compile(r"RSSI (-\d\d)")
+    rssi = rssiPattern.search(data).group(1)
+
+    # print(f"Packet: {packet}")
+    print(f"Temperature: {temperature}")
+    print(f"Pressure: {pressure}")
+    print(f"Altitude: {altitude}")
+    print(f"RSSI: {rssi}")
+    
+    # Close the serial port connection
+    ser.close()
+
+except Exception as e:
+    print(f'Error occurred: {e}')
