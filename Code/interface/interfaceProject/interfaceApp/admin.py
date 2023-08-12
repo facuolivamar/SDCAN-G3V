3eb669ea21dc1fad6638d7d75d8eaf0074b6f255
facuolivamar <facundoolivam@gmail.com>
2023-08-12 13:58:18 -0300

    models input class

--- Code/interface/interfaceProject/interfaceApp/admin.py
+++ Code/interface/interfaceProject/interfaceApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import input
+
 # Register your models here.
+
+admin.site.register(input)
--- Code/interface/interfaceProject/interfaceApp/models.py
+++ Code/interface/interfaceProject/interfaceApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class input(models.Model):
+    serial_port = models.CharField(max_length=255, blank=False, null=False, default='COM3')
+    baud_rate = models.IntegerField(blank=False, null=False, default=115200)
+    timeout = models.IntegerField(blank=False, null=False, default=1)
+    
+    estacion_terrena_latitude = models.FloatField(blank=False, null=False)
+    estacion_terrena_longitude = models.FloatField(blank=False, null=False)
+
+    # history = HistoricalRecords()
+
+    def __str__(self):
+        return f"Serial port reading: {self.serial_port}, baud rate: {self.baud_rate}, timeout: {self.timeout}, estacion terrena latitude: {self.estacion_terrena_latitude}, estacion terrena longitude: {self.estacion_terrena_longitude}."
