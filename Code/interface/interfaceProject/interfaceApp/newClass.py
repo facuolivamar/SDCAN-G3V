2e1a43ca98d195f565142c683035c462a4f863fe
facuolivamar <facundoolivam@gmail.com>
2023-09-27 13:24:21 -0300

    data to csv

--- Code/interface/interfaceProject/interfaceApp/newClass.py
+++ Code/interface/interfaceProject/interfaceApp/newClass.py
@@ -5,11 +5,18 @@ import pandas as pd
 import re
 import time
 import datetime
+import os
+
+csv_filename = 'data-receiver-150ms.csv'
+csv_directory = 'C:/Users/facun/OneDrive/Documentos/Extra Projects/SDCAN-G3V/Code/interface/interfaceProject/interfaceApp'
+csv_path = os.path.join(csv_directory, csv_filename)
+
+xlsx_filename = 'data-receiver-150ms.xlsx'
+xlsx_directory = 'C:/Users/facun/OneDrive/Documentos/Extra Projects/SDCAN-G3V/Code/interface/interfaceProject/interfaceApp'
+xlsx_path = os.path.join(xlsx_directory, xlsx_filename)
 
 async def read_serial_data():
     ser = serial.Serial('COM3', 115200, timeout=1)
-    count = 0
-    dataDict = {}
     
     while True:
         if not ser.isOpen():
@@ -17,56 +24,56 @@ async def read_serial_data():
 
         try:
             data = ser.read(1000).decode('utf-8')
-            dataDict[time.ctime(time.time()) ] = data
             
             # Define a regular expression pattern to capture the values
             pattern = re.compile(r"Received packet.*?(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+)\s+(\d+)\s+(\d+\.\d+)", re.DOTALL)
 
             # Find all matches in the text
-            matches = pattern.findall(data )
+            matches = pattern.findall(data)
 
-            # Define the column names
-            columns = ["dust", "mq135", "mq131", "mq4", "temperature", "pressure", "altitude"]
+            # Create a list of dictionaries to hold the data
+            data_list = []
 
-            # Print the header
-            print(", ".join(columns))
-
-            print(matches)
-            # Print the captured values
+            # Process the matches and append them to the list
             for match in matches:
-                # Convert the values to strings and join them with commas
-                row = ", ".join(map(str, match))
-                print(row)
-                
-            
+                current_datetime = time.ctime(time.time())
+                data_dict = {
+                    "datetime": current_datetime,
+                    "dust": int(match[0]),
+                    "mq135": int(match[1]),
+                    "mq131": int(match[2]),
+                    "mq4": int(match[3]),
+                    "temperature": float(match[4]),
+                    "pressure": int(match[5]),
+                    "altitude": float(match[6])
+                }
+                data_list.append(data_dict)
+
+            # Create a DataFrame from the list of dictionaries
+            df = pd.DataFrame(data_list)
+
+            # Append the DataFrame to an existing CSV file or create a new one
+            if os.path.exists(csv_path):
+                df.to_csv(csv_path, mode='a', header=False, index=False)
+            else:
+                df.to_csv(csv_path, index=False)
 
 
-            count += 1
-            if count >= 10:
-                print(f'count: {count}')
-                print(time.time() - varTime)
-                break
         except UnicodeDecodeError as e:
             print(f'UnicodeDecodeError: {e}')
 
         ser.close()
 
-# Store the start time
-varTime = time.time()
-
 # Create an asyncio event loop
 loop = asyncio.get_event_loop()
 
-# Schedule the serial data reading coroutine to run every 150 milliseconds
+# Schedule the serial data reading coroutine to run indefinitely
 try:
     loop.run_until_complete(asyncio.gather(
         read_serial_data(),
-        asyncio.sleep(0.15),  # Sleep for 150 milliseconds
     ))
 except KeyboardInterrupt:
     pass
 
 # Close the event loop
 loop.close()
-
-exit()
